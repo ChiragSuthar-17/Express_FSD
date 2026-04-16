194fb769bee4f6e05743f690bb88c1f6a2390d5a
ChiragSuthar-17 <chirag.suthar1313@gmail.com>
2026-04-16 11:08:55 +0530

    updates files

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,14 @@
+const errorHandler = (err, req, res, next) => {
+    console.log(err.stack);
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+
+    res.staus(statusCode).json({
+        success: false,
+        message,
+        ...err[process.env.NODE_ENV === 'development' && {stack: err.stack}]
+    });
+};
+
+module.exports = errorHandler;
